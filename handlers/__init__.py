2c9a9839a54511ecf12a0d4c60a230874826e27e
Abdumannon Rabbimqulov <abdumannonrab@icloud.com>
2026-05-19 17:26:04 +0500

    bot

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,12 +1,8 @@
 from aiogram import Router
 from .start import router as start_router
-from .location import router as location_router
 from .verification_code import router as verification_code_router
 
-# Base router for all handlers
 main_router = Router()
 
-# Include sub-routers (Order matters!)
 main_router.include_router(start_router)
-main_router.include_router(location_router)
 main_router.include_router(verification_code_router)
